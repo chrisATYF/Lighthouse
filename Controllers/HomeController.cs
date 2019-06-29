12ac6b5681ab255e69622e78d9111ba5ed102f5d
chrisATYF <36751474+chrisATYF@users.noreply.github.com>
2019-06-29 17:20:22 -0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lighthouse.Controllers
+{
+    [RoutePrefix("Home")]
+    public class HomeController : Controller
+    {
+        [Route("", Name = "HomeIndex")]
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToRoute("");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lighthouse.Startup))]
+namespace Lighthouse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
